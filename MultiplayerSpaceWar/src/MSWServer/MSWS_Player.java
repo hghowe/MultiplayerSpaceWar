831688d6310d70685717c96e7ec4abb0a6ec6135
hghowe <harlan.howe@kinkaid.org>
2017-05-04 08:18:53 -0500

    Initial commit of MSWS_Player
    (Really, this time.)

---
+++ MultiplayerSpaceWar/src/MSWServer/MSWS_Player.java
@@ -0,0 +1,13 @@
+/**
+ * This class represents one of the players in the game, as used by the MSWServer class.
+ * If you are writing a client class, write your own!
+ * @author harlan.howe
+ *
+ */
+
+package MSWServer;
+
+public class MSWS_Player
+{
+
+}
