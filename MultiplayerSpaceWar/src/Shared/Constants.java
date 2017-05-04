6747b413af74fd703071dafe723be5920d834ec6
hghowe <harlan.howe@kinkaid.org>
2017-05-04 15:41:28 -0500

    updated constants for screen size and player control effects

--- MultiplayerSpaceWar/src/Shared/Constants.java
+++ MultiplayerSpaceWar/src/Shared/Constants.java
@@ -1,5 +1,21 @@
+package Shared;
 
 public interface Constants
 {
-
+	// Broadcast constants...
+	final String[] messageTypes = {"NEW_PLAYER","UPDATE","PLAYER_LIST","PLAYER_LEAVING","USER_CONTROLS","DISPLAY"};
+	final int NEW_PLAYER_MESSAGE_TYPE     = 0;
+	final int UPDATE_MESSAGE_TYPE 	  	  = 1;
+	final int PLAYER_LIST_MESSAGE_TYPE    = 2;
+	final int PLAYER_LEAVING_MESSAGE_TYPE = 3;
+	final int USER_CONTROLS_MESSAGE_TYPE  = 4;
+	final int DISPLAY_MESSAGE_TYPE        = 5;
+	
+	final int SCREEN_WIDTH = 800;
+	final int SCREEN_HEIGHT = 800;
+	
+	final double ANGULAR_ACCELERATION_OF_PLAYER = Math.PI/15;
+	final double THRUST_OF_PLAYER = 10.0;
+	
+	
 }
