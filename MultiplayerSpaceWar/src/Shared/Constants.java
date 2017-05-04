ee6c98b91984c3004a23965b2f345307ca7a442c
hghowe <harlan.howe@kinkaid.org>
2017-05-04 15:43:39 -0500

    Added user command masks

--- MultiplayerSpaceWar/src/Shared/Constants.java
+++ MultiplayerSpaceWar/src/Shared/Constants.java
@@ -17,5 +17,10 @@ public interface Constants
 	final double ANGULAR_ACCELERATION_OF_PLAYER = Math.PI/15;
 	final double THRUST_OF_PLAYER = 10.0;
 	
+	final int TURN_LEFT_COMMAND = 1;
+	final int TURN_RIGHT_COMMAND = 2;
+	final int THRUST_COMMAND = 4;
+	final int FIRE_COMMAND = 8;
+	final int USE_POWERUP_COMMAND = 16;
 	
 }
