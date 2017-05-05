6563e53546124b25b7d7c2b5679de3a1cca2b7f6
hghowe <harlan.howe@kinkaid.org>
2017-05-04 22:02:21 -0500

    Created Projectile class
    Added Projectile-related constants

---
+++ MultiplayerSpaceWar/src/MSWServer/MSWS_Projectile.java
@@ -0,0 +1,12 @@
+package MSWServer;
+
+public class MSWS_Projectile extends GameElement
+{
+
+	public MSWS_Projectile(double x, double y, double angle, double xV, double yV)
+	{
+		super(x, y, angle, xV, yV);
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- MultiplayerSpaceWar/src/Shared/Constants.java
+++ MultiplayerSpaceWar/src/Shared/Constants.java
@@ -16,9 +16,11 @@ public interface Constants
 	
 	final double ANGULAR_ACCELERATION_OF_PLAYER = Math.PI/15;
 	final double THRUST_OF_PLAYER = 10.0;
-	final double PLAYER_MAX_VELOCITY = 50;
+	final double PLAYER_MAX_VELOCITY = 50.0;
 	final double PLAYER_MAX_VELOCITY_SQUARED = 2500;
 	
+	final double PROJECTILE_MUZZLE_VELOCITY = 75.0;
+	final double PROJECTILE_LIFETIME = 1.5; // seconds
 	
 	final int TURN_LEFT_COMMAND = 1;
 	final int TURN_RIGHT_COMMAND = 2;
