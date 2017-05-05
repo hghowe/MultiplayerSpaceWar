ca6a24f7d985e7de755f4de5cbb6e9eaa521ae83
hghowe <harlan.howe@kinkaid.org>
2017-05-05 07:38:43 -0500

    MSW_GameRunner & MSWPlayerPanel origin

---
+++ MultiplayerSpaceWar/src/MSWClient/MSWPlayerPanel.java
@@ -0,0 +1,21 @@
+package MSWClient;
+
+import java.awt.LayoutManager;
+
+import javax.swing.JPanel;
+
+public class MSWPlayerPanel extends JPanel implements Shared.Constants
+{
+
+	public MSWPlayerPanel()
+	{
+		super();
+	}
+
+	public void beginGame()
+	{
+		
+		
+	}
+
+}
---
+++ MultiplayerSpaceWar/src/MSWClient/MSW_GameRunner.java
@@ -0,0 +1,31 @@
+package MSWClient;
+
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+
+public class MSW_GameRunner extends JFrame implements Shared.Constants
+{
+	private MSWPlayerPanel thePanel;
+	
+	public static void main(String[] args)
+	{
+		MSW_GameRunner theApp = new MSW_GameRunner();
+	}
+	
+	public MSW_GameRunner()
+	{
+		super("Multiplayer Space War!");
+		setSize(SCREEN_WIDTH,SCREEN_HEIGHT+16);
+		getContentPane().setLayout(new GridLayout(1,1));
+		thePanel = new MSWPlayerPanel();
+		getContentPane().add(thePanel);
+		setResizable(false);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setVisible(true);
+		thePanel.beginGame();
+	}
+	
+
+}
