8d4b66b61c2411d3a82d788177f19b0c75a25081
hghowe <harlan.howe@kinkaid.org>
2017-05-04 21:53:01 -0500

    Fleshed out game loop into steps
    Wrote method stubs.
    Wrote overloaded broadcast method.

--- MultiplayerSpaceWar/src/MSWServer/MSWServer.java
+++ MultiplayerSpaceWar/src/MSWServer/MSWServer.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -21,6 +23,10 @@ public class MSWServer extends TimerTask implements Shared.Constants
 	private Map<Integer, MSWS_Player> players;
 	private Date lastUpdate;
 	
+	private List<GameElement> gameElements;
+	
+	
+	
 	public MSWServer()
 	{
 		super();
@@ -31,7 +37,7 @@ public class MSWServer extends TimerTask implements Shared.Constants
 		players = new HashMap<Integer, MSWS_Player>();
 		setupNetworking();
 		
-		
+		gameElements = new ArrayList<GameElement>();
 		
 		
 		
@@ -52,6 +58,7 @@ public class MSWServer extends TimerTask implements Shared.Constants
 				ClientReader cr = new ClientReader(clientSocket, pw);
 				MSWS_Player nextPlayer = new MSWS_Player(cr.getName(),nextAvailableID, pw);
 				players.put(nextAvailableID, nextPlayer);
+				gameElements.add(nextPlayer);
 				broadcast(NEW_PLAYER_MESSAGE_TYPE,new String[]{""+nextAvailableID, cr.getName()});
 				sendPlayerList();
 				nextAvailableID++;
@@ -74,9 +81,64 @@ public class MSWServer extends TimerTask implements Shared.Constants
 		double dT = (currentTime.getTime() - lastUpdate.getTime()) / 1000.0;
 		lastUpdate = currentTime;
 		
+		plan(dt);
+		move(dt);
+		detect();
+		prune();
+		announce();
+			
+	}
+	
+	/**
+	 * handle all pre-move decisions, such as spawning bullets, powerups, etc.
+	 */
+	public void plan(double dt)
+	{
+		// nothing yet... will be spawning.
 		
+	}
+	/**
+	 * change the locations of all the gameElements, as needed.
+	 */
+	public void move(double dt)
+	{
 		
+	}
+	
+	/**
+	 * check for collisions, end-of-life timers; potentially tell game elements to die or to change a state.
+	 */
+	public void detect()
+	{
 		
+	}
+	
+	/**
+	 * go through all the game elements and remove any one that isDead from all lists.
+	 */
+	public void prune()
+	{
+		
+	}
+	
+	/**
+	 * build the status string and broadcast it.
+	 */
+	public void announce()
+	{
+		
+	}
+	
+	/**
+	 * send the message type string and the long param to all players.
+	 * @param messageType
+	 * @param longParam
+	 */
+	public void broadcast(int messageType, String longParam)
+	{
+		String message = messageTypes[messageType]+"\t"+longParam;
+		for (Integer id: players.keySet())
+			players.get(id).sendMessage(message);
 	}
 	
 	public void broadcast(int messageType, String[] params)
@@ -102,6 +164,7 @@ public class MSWServer extends TimerTask implements Shared.Constants
 	{
 		System.out.println("Disconnecting "+whichID);
 		broadcast(PLAYER_LEAVING_MESSAGE_TYPE, new String[] {players.get(whichID).getName()});
+		gameElements.remove(players.get(whichID));
 		players.remove(whichID);
 		this.sendPlayerList();
 	}
