724b05806dbffc58966066aa2f7be64707cf571d
hghowe <harlan.howe@kinkaid.org>
2017-05-04 22:02:47 -0500

    MSWS_Projectile origin

--- MultiplayerSpaceWar/src/MSWServer/MSWS_Projectile.java
+++ MultiplayerSpaceWar/src/MSWServer/MSWS_Projectile.java
@@ -2,11 +2,20 @@ package MSWServer;
 
 public class MSWS_Projectile extends GameElement
 {
-
+	private double age;
 	public MSWS_Projectile(double x, double y, double angle, double xV, double yV)
 	{
 		super(x, y, angle, xV, yV);
-		// TODO Auto-generated constructor stub
+		age = 0.0;
+		radius = 1;
+	}
+	
+	public void makeMove(double dT)
+	{
+		super.makeMove(dT);
+		age+= dT;
+		if (age>PROJECTILE_LIFETIME)
+			die();
 	}
 
 }
